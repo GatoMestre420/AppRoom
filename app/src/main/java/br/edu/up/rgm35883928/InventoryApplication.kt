a4a943c30569f2d4a06f4d64ab8b0cb621a61a6f
GatoMestre420 <luancurt@outlook.com>
2024-11-25 18:53:01 -0300

    HomeScreem

---
+++ app/src/main/java/br/edu/up/rgm35883928/InventoryApplication.kt
@@ -0,0 +1,17 @@
+package br.edu.up.rgm35883928
+
+import android.app.Application
+
+
+class InventoryApplication : Application() {
+
+    /**
+     * AppContainer instance used by the rest of classes to obtain dependencies
+     */
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppDataContainer(this)
+    }
+}
